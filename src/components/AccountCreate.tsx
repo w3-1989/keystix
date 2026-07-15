bf327f91399780b62ea00ceb300a9a88d84492dc
w3-1989 <charlie.james.e.p@gmail.com>
2026-07-15 12:45:04 +0100

    feat: integrate Headless UI for role selection and add SVG support
    - Added @headlessui/react for improved UI components.
    - Implemented a Listbox for role selection in AccountCreate component.
    - Introduced @svgr/cli and vite-plugin-svgr for SVG handling.
    - Added a new SVG icon for branding.
    - Updated form styling and structure for better user experience.
    - Enhanced CSS variables for colors and fonts in index.css.

--- src/components/AccountCreate.tsx
+++ src/components/AccountCreate.tsx
@@ -1,6 +1,22 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, type SubmitHandler } from "react-hook-form";
+import Icon from "../assets/brand/icon_lb.svg?react";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from "@headlessui/react";
+import { Controller } from "react-hook-form";
 import { z } from "zod";
+
+const roles = [
+  "CEO / Founder",
+  "COO (Chief Operating Officer)",
+  "Head of Operations",
+  "Business Development Manager",
+  "Executive Assistant / Chief of Staff",
+] as const;
 
 const formSchema = z
   .object({
@@ -37,6 +53,7 @@ type FormFields = z.infer<typeof formSchema>;
 export default function AccountCreate() {
   const {
     register,
+    control,
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<FormFields>({
@@ -48,107 +65,146 @@ export default function AccountCreate() {
   };
 
   return (
-    <main className="flex justify-center align-center h-screen">
+    <main className="flex flex-col items-center justify-center h-screen w-fill">
+      <div className="flex flex-col justify-center items-center gap-2 mb-4">
+        <Icon className="w-12 h-12 mb-2" />
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="font-adelphi font-medium uppercase text-[24px]">
+            Your World. Your System.
+          </h1>
+          <p className="font-dm-sans font-normal text-brand-grey-200 text-[16px]">
+            Create your Keystix account{" "}
+          </p>
+        </div>
+      </div>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col justify-center gap-4"
+        className="flex flex-col justify-center gap-4 max-w-[360px] "
       >
         <div className="flex flex-row gap-4">
-          <label className="flex flex-col text-brand-grey-200 ">
+          <label className="flex flex-col text-brand-grey-200 text-[12px]">
             First Name
             <input
               {...register("firstName")}
               type="text"
               placeholder="John"
-              className="p-3 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg"
+              className="p-2 mt-2 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-light-blue-300"
             />
             {errors.firstName && (
-              <div className="text-red-500">{errors.firstName.message}</div>
+              <div className="text-red-500 mt-0.5">
+                {errors.firstName.message}
+              </div>
             )}
           </label>
 
-          <label className="flex flex-col text-brand-grey-200">
+          <label className="flex flex-col text-brand-grey-200 text-[12px]">
             Last Name
             <input
               {...register("lastName")}
               type="text"
               placeholder="Doe"
-              className="p-3 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg"
+              className="p-2 mt-2 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-light-blue-300"
             />
             {errors.lastName && (
-              <div className="text-red-500">{errors.lastName.message}</div>
+              <div className="text-red-500 mt-0.5">
+                {errors.lastName.message}
+              </div>
             )}
           </label>
         </div>
-        <label className="flex flex-col text-brand-grey-200">
+        <label className="flex flex-col text-brand-grey-200 text-[12px]">
           Company Email
           <input
             {...register("companyEmail")}
             type="text"
             placeholder="Enter your email address..."
-            className="p-3 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg"
+            className="p-2 mt-2 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-light-blue-300"
           />
           {errors.companyEmail && (
-            <div className="text-red-500">{errors.companyEmail.message}</div>
+            <div className="text-red-500 mt-0.5">
+              {errors.companyEmail.message}
+            </div>
           )}
         </label>
 
-        <label className="flex flex-col text-brand-grey-200">
+        <label className="flex flex-col text-brand-grey-200 text-[12px]">
           Role
-          <select
-            {...register("role")}
+          <Controller
             name="role"
-            id="role"
-            defaultValue="Select your role"
-            className="p-3 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg"
-          >
-            <option value="Select your role" disabled>
-              Select your role
-            </option>
-            <option value="CEO / Founder">CEO / Founder</option>
-            <option value="COO (Chief Operating Officer)">
-              COO (Chief Operating Officer)
-            </option>
-            <option value="Head of Operations">Head of Operations</option>
-            <option value="Business Development Manager">
-              Business Development Manager
-            </option>
-            <option value="Executive Assistant / Chief of Staff">
-              Executive Assistant / Chief of Staff
-            </option>
-          </select>
+            control={control}
+            render={({ field }) => (
+              <Listbox value={field.value} onChange={field.onChange}>
+                <ListboxButton className="w-full mt-2 p-2 border border-brand-grey-100 rounded-lg text-left flex justify-between items-center focus:outline-none focus:ring-1 focus:ring-brand-light-blue-300">
+                  <span
+                    className={
+                      field.value ? "text-black " : "text-brand-grey-200"
+                    }
+                  >
+                    {field.value || "Select your role"}
+                  </span>
+                </ListboxButton>
+                <ListboxOptions
+                  anchor="bottom start"
+                  className=" w-(--button-width) mt-2 rounded-xl border border-brand-grey-100 bg-white shadow-lg p-2 space-y-1 focus:outline-none"
+                >
+                  {roles.map((role) => (
+                    <ListboxOption
+                      key={role}
+                      value={role}
+                      className="px-3 py-2 text-[12px] rounded-lg cursor-pointer data-focus:bg-brand-light-blue-300/10"
+                    >
+                      {role}
+                    </ListboxOption>
+                  ))}
+                </ListboxOptions>
+              </Listbox>
+            )}
+          />
           {errors.role && (
-            <div className="text-red-500">{errors.role.message}</div>
+            <div className="text-red-500 mt-0.5">{errors.role.message}</div>
           )}
         </label>
 
-        <label className="flex flex-col text-brand-grey-200">
-          Password
+        <label className="flex flex-col text-brand-grey-200 text-[12px]">
+          Password  
           <input
             {...register("password")}
-            type="text"
+            type="password"
             placeholder="Enter your password"
-            className="p-3 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg"
+            className="p-2 mt-2 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-light-blue-300"
           />
           {errors.password && (
-            <div className="text-red-500">{errors.password.message}</div>
+            <div className="text-red-500 mt-0.5">{errors.password.message}</div>
           )}
         </label>
-        <label className="flex flex-col text-brand-grey-200">
+        <label className="flex flex-col text-brand-grey-200 text-[12px]">
           Confirm Password
           <input
             {...register("confirmPassword")}
-            type="text"
+            type="password"
             placeholder="Confirm your password..."
-            className="p-3 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg"
+            className="p-2 mt-2 border-1 text-black placeholder:text-brand-grey-200 border-brand-grey-100 rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-light-blue-300"
           />
           {errors.confirmPassword && (
-            <div className="text-red-500">{errors.confirmPassword.message}</div>
+            <div className="text-red-500 mt-0.5 ">
+              {errors.confirmPassword.message}
+            </div>
           )}
         </label>
-        <button className="w-full h-[48px] bg-brand-light-blue-300 text-white rounded-lg" type="submit" disabled={isSubmitting}>
+        <button
+          className="w-full text-[12px] h-[40px] bg-brand-light-blue-300 text-white rounded-lg"
+          type="submit"
+          disabled={isSubmitting}
+        >
           {isSubmitting ? "Loading..." : "Continue"}
         </button>
+        <div className="flex justify-center text-center text-brand-grey-200">
+          <p className="text-[12px] max-w-[] ">
+            By continuing, you acknowledge that you understand and agree to the{" "}
+            <span className="underline cursor-pointer">Terms & Conditions</span>{" "}
+            and <span className="underline cursor-pointer">Privacy Policy</span>
+          </p>
+        </div>
       </form>
     </main>
   );
